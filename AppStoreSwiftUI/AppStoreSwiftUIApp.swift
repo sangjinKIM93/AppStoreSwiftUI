bbcc78e3fa4a2cf6af91c7537ede5cf0c48b996d
sangjinKIM93 <larger611@gmail.com>
2023-03-29 21:48:02 +0900

    Initial Commit

---
+++ AppStoreSwiftUI/AppStoreSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreSwiftUIApp.swift
+//  AppStoreSwiftUI
+//
+//  Created by 김상진 on 2023/03/29.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
